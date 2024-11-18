a89528b7ae03e54c16cf874858b6db3700cd26b6
Plasencia2006 <plasenciaalvahectorisai@gmail.com>
2024-11-17 22:12:50 -0500

    hola_ mundo

---
+++ ejecico.js
@@ -0,0 +1,12 @@
+
+const a = 10; 
+const b = 15; 
+
+
+if (a > b) {
+    console.log(`El número ${a} es mayor que ${b}.`);
+} else if (a < b) {
+    console.log(`El número ${a} es menor que ${b}.`);
+} else {
+    console.log(`El número ${a} es igual a ${b}.`);
+}
